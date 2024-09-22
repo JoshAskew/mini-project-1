c9c65f70f675c15d3affdb2f2dec3b098751f138
JoshAskew <asksprit3@gmail.com>
2024-09-22 12:42:56 -0500

    added JS file and code

--- java.js
+++ java.js
@@ -1,9 +1,10 @@
-document.getElementById('flexboxCode').addEventListener('click', function() {
-    const codeText = this.innerText.trim();
+function copyToClipboard(code) {
+    const codeElement = document.getElementById(code);
+    const codeText = codeElement.innerText;
 
     navigator.clipboard.writeText(codeText).then(() => {
         alert('Code copied to clipboard!');
     }).catch(err => {
-        console.error('Could not copy text: ', err);
+        console.error('Failed to copy: ', err);
     });
-});
+}
